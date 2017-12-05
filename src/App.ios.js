b9d143563e3098530dbe8f376c6b8ed68cfee888
msantam2 <msantam2@gmail.com>
2017-12-04 22:58:06 -0800

    Add src/ folder and setup initial render.

---
+++ src/App.ios.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => (
+  <View>
+    <Text>
+      hello TV!
+    </Text>
+  </View>
+);
+
+export default App;
