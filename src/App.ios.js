c612240fec0c6dd1630f0a581bb2a9ec86fa484a
msantam2 <msantam2@gmail.com>
2017-12-04 23:29:20 -0800

    Configure dev at launch.

--- src/App.ios.js
+++ src/App.ios.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import Firebase from 'firebase';
+import initFirebase from './initialization/firebase';
 
-const App = () => (
-  <View>
-    <Text>
-      hello TV!
-    </Text>
-  </View>
-);
+class App extends React.Component {
+  static configureDev() {
+    console.ignoredYellowBox = ['Remote debugger'];
+  }
+
+  constructor(props) {
+    super(props);
+    initFirebase();
+
+    if (process.env.NODE_ENV === 'development') {
+      App.configureDev();
+    }
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>
+          hello again TV!
+        </Text>
+      </View>
+    );
+  }
+}
 
 export default App;
