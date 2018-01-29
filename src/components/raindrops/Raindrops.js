02dc63415eaec2fad523a5afcbd299c83e813bcf
msantam2 <msantam2@gmail.com>
2018-01-28 18:29:24 -0800

    Update Raindrops component to attach listener to correct node of Realtime Database and get the downloadURL, and display the image within this component.

--- src/components/raindrops/Raindrops.js
+++ src/components/raindrops/Raindrops.js
@@ -1,10 +1,42 @@
 import React from 'react';
-import { Text } from 'react-native';
+import Firebase from 'firebase';
+import { Image, Text } from 'react-native';
 
-const Raindrops = () => (
-  <Text>
-    Raindrops
-  </Text>
-);
+class Raindrops extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { imageURL: null };
+  }
+
+  componentDidMount() {
+    let { user } = this.props;
+    user = user || Firebase.auth().currentUser;
+
+    const db = Firebase.database();
+    const raindropsRef = db.ref(`users/${user.uid}`);
+
+    raindropsRef.on('child_added', (snapshot) => {
+      const newImage = snapshot.val();
+      this.setState({ imageURL: newImage.downloadURL });
+    });
+  }
+
+  render() {
+    if (this.state.imageURL) {
+      return (
+        <Image
+          style={{ flex: 1 }}
+          source={{ uri: this.state.imageURL }}
+        />
+      );
+    }
+
+    return (
+      <Text>
+        Raindrops
+      </Text>
+    );
+  }
+}
 
 export default Raindrops;
